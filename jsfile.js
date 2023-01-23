a8184100fc23466d1053992f28e7320d4543a862
Cisco-29 <francis.paddock@icloud.com>
2023-01-24 09:46:22 +1100

    Add full grid size change functionality

--- jsfile.js
+++ jsfile.js
@@ -2,23 +2,58 @@
 
 const container = document.getElementById("content");
 container.style.maxWidth = "800px";
-container.style.width = "800px";
+const canvasWidth = container.style.width = "800px";
 container.style.maxHeight = "800px";
-container.style.height = "800px";
+const canvasHeight =container.style.height = "800px";
 container.style.border = "2px solid black";
 container.style.display = "flex";
 container.style.flexDirection = "column";
 
 let allRowsArr = [];
+let gridSize;
+let boxSize;
 
-createRows();
+// reference and link to button
+const gridSizeBtn = document.querySelector('#gridSizeBtn');
+gridSizeBtn.onclick = () => getSetGridSize();
 
-function createRows(){
+function getSetGridSize(){
+    // prompt user for grid size
+    let proposedGridSize = prompt("Set a grid size between 4 and 100 \neg. 4 = 4 x 4 grid, 10 = 10 x 10 grid");
+    if (proposedGridSize > 100 || proposedGridSize < 4){
+        alert("Grid size must be between 4 and 100!");
+        return;
+    }else{
+        gridSize = proposedGridSize;
+        render(gridSize);
+    }
+}
 
-    for(let i = 0; i < 16; i++){
+
+function render(gridDimensions){
+    removeGrid();
+    boxSize = 800/gridDimensions;
+    createRows(gridDimensions);
+    
+}
+function removeGrid(){
+
+    let elementsToRemove = document.getElementsByClassName("row");
+    if (container.hasChildNodes())
+    {   do {
+            container.removeChild(elementsToRemove[0]);
+
+        }while (container.hasChildNodes())
+    }
+}
+
+function createRows(gridDimensions){
+
+    for(let i = 0; i < gridDimensions; i++){
         // creation and styling
         let row = document.createElement("div");
-        row.style.height = "50px";
+        row.style.height = boxSize + "px"
+        console.log(row.style.height);
         row.style.display = "flex";
         row.classList.add("row");
 
@@ -30,33 +65,22 @@ function createRows(){
         // add row to array
         allRowsArr.push(row);
         
-        createBoxes(row);
+        createBoxes(row, gridDimensions);
     }
     
 }
 
-function createBoxes(row) {
-    for (j = 0; j < 16; j++) {
+function createBoxes(row, gridDimensions) {
+    for (j = 0; j < gridDimensions; j++) {
         let box = document.createElement("div");
-        box.style.height = "50px";
-        box.style.width = "50px";
+        box.style.height = boxSize + "px"
+        box.style.width = boxSize + "px"
         box.style.border = "0.25px solid grey";
         box.classList.add("box");
-
-
-
         row.appendChild(box);
     }
 }
 
-        // hover funtionality goes here
-        
 
 
 
-
-
-
-
-
-        // -----------------------------
